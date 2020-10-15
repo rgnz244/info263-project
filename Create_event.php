78f8f92cd676aafa1e9c3d7fda165f66d897ba85
rgnz244 <71376366+rgnz244@users.noreply.github.com>
2020-10-15 21:05:22 +1300

    Update Create_event.php

--- Create_event.php
+++ Create_event.php
@@ -2,20 +2,12 @@
 <?php
 //Header and navigation panel
 include "Header.php";
-
-//SQL connection
-$mysqli = new mysqli("132.181.143.31","mhp47","Pnxr367_","INFO263_mhp47_tserver");
-if ($mysqli->connect_error) {
-    echo $mysqli->connect_error;
-}
+require_once "function-include.php";
+require_once "config.php";
 
 
-$cluster_query = "CALL show_clusters()";
-$resultCluster = $mysqli->query($cluster_query);
-
-$room_query = "CALL show_rooms()";
-$resultRooms = $mysqli->query($room_query);
-
+$result_cluster = mysqli_query($conn, "SELECT cluster_name FROM front_cluster;");
+$result_rooms = mysqli_query($conn, "SELECT room_name FROM front_room ORDER BY room_name");
 
 
 ?>
@@ -25,19 +17,21 @@ $resultRooms = $mysqli->query($room_query);
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
 
-
-
 <div class = "Name">
     <div class="col-md-6 col-sm-12">
         <div class = "Create-event-form" method = "post" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             <form action="" method="post">
                 <div class="form-event">
-                    <label>Event: </label>
-                    <select name="Courses">
+                    <label>Event name:</label>
+                    <input type="text" name="event_name">
+                </div>
+                <div class="form-event">
+                    <label>Cluster: </label>
+                    <select name="select_cluster">
                         <?php
-                        while($col=mysqli_fetch_assoc($resultCluster)){
-                            $course_code = $col['cluster_name'];
-                            echo "<option value='".$course_code."'>".$course_code."</option>";
+                        while($col=mysqli_fetch_assoc($result_cluster)){
+                            $cluster = $col['cluster_name'];
+                            echo "<option value='".$cluster."'>".$cluster."</option>";
                         }
                         ?>
                     </select>
@@ -47,7 +41,7 @@ $resultRooms = $mysqli->query($room_query);
                     <label>Room:   </label>
                     <select name="Room">
                         <?php
-                        while($col=mysqli_fetch_assoc($resultRooms)){
+                        while($col=mysqli_fetch_assoc($result_rooms)){
                             $room_code = $col['room_name'];
                             echo "<option value='".$room_code."'>".$room_code."</option>";
                         }
@@ -57,22 +51,46 @@ $resultRooms = $mysqli->query($room_query);
 
                 <div class="form-event">
                     <label>Date</label>
-                    <input type="date" id="datefield" name="event-date"
+                    <input type="date" id="datefield" name="date"
                            min="today" max="2050-12-31">
                     </input>
                 </div>
 
                 <button type="submit" class="btn btn-black" name="btn_create">Create</button>
+                <?php
+                if (isset($_POST['btn_create'])){
+                    $event_name=$_POST['event_name'];
+                    create_event($conn,$event_name);
+                }
+                ?>
+
             </form>
         </div>
     </div>
 </div>
 
+<?php
 
+
+function create_event($conn, $event_name)
+{
+    $sql = "INSERT INTO INFO263_mhp47_tserver.front_event (event_name, status)  VALUES (?,?);";
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt,$sql)){
+        exit();
+    }
+
+    $status = 1;
+    mysqli_stmt_bind_param($stmt,"sd", $event_name, $status);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    exit();
+
+}
+
+?>
 
 
 
 
 </html>
-
-
